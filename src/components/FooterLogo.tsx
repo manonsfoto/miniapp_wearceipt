4466cfbd70eefa268abb38fa883122b1571e5f06
Minyeong Jeong <megormin@gmail.com>
2025-02-27 14:32:03 +0100

    first commit

---
+++ src/components/FooterLogo.tsx
@@ -0,0 +1,14 @@
+import LogoIcon from "../assets/SVG/LogoIcon";
+
+const FooterLogo = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <LogoIcon />
+      <p className="font-Modak-Regular text-2xl capitalize -mt-2 tracking-wide">
+        Wearceipt
+      </p>
+    </div>
+  );
+};
+
+export default FooterLogo;
